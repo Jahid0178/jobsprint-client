13df6de4ae6ee13614924778ff53024c2f4b47d1
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-27 19:18:19 +0600

    feat: Add validation schema for user registration form
    - Implemented `registerFormSchema` using `zod` for validating registration form inputs.
    - Added field validations:
      - `fullName`: Must be at least 3 characters.
      - `email`: Must be a valid email format.
      - `password`: Must be at least 6 characters.
      - `confirmPassword`: Must match the `password` field.
    - Included a custom refinement to ensure passwords match with appropriate error messaging.
    - Enhanced modularity for consistent validation across the application.

--- src/validation/validations.ts
+++ src/validation/validations.ts
@@ -6,3 +6,21 @@ export const loginFormSchema = z.object({
     .string()
     .min(6, { message: "Password must be at least 6 characters" }),
 });
+
+export const registerFormSchema = z
+  .object({
+    fullName: z
+      .string()
+      .min(3, { message: "Full name must be at least 3 characters" }),
+    email: z.string().email({ message: "Invalid email address" }),
+    password: z
+      .string()
+      .min(6, { message: "Password must be at least 6 characters" }),
+    confirmPassword: z.string().min(6, {
+      message: "Password must be at least 6 characters",
+    }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });

91b11a80d0a2debecac69fb6fdf997bf93a1c5c5
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-28 01:17:28 +0600

    feat: Add display of job listings with filter functionality to App component
    - Integrated `JobFiltersPanel` for filtering job listings.
    - Updated layout to include a responsive grid for displaying multiple `JobListingCard` components.
    - Enhanced user interface for listing and filtering jobs effectively.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import JobFiltersPanel from "./components/Filters/JobFiltersPanel";
+import JobListingCard from "./components/JobListingCard/JobListingCard";
+
 const App = () => {
-  return <div>App Component</div>;
+  return (
+    <section>
+      <div className="container">
+        <div className="flex justify-between items-center gap-4 mb-5">
+          <h2 className="text-2xl font-semibold">All Job Listings</h2>
+          <JobFiltersPanel />
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
+          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16].map(
+            (item) => (
+              <JobListingCard key={item} />
+            )
+          )}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default App;

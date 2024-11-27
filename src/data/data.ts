b0d759de141ebf2d56797657c8a736e2f40566c9
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-27 21:05:59 +0600

    feat: Add job types dataset in data.ts
    - Created a `jobTypes` array to store predefined job type options.
    - Each job type includes:
      - `id`: Unique identifier.
      - `title`: Human-readable job type label (e.g., "Full Time").
      - `value`: String representation for backend or form integration.
    - Ensures consistent and reusable job type options across the application.

---
+++ src/data/data.ts
@@ -0,0 +1,19 @@
+import { JobType } from "@/typescript/type";
+
+export const jobTypes: JobType[] = [
+  {
+    id: 1,
+    title: "Full Time",
+    value: "full-time",
+  },
+  {
+    id: 2,
+    title: "Part Time",
+    value: "part-time",
+  },
+  {
+    id: 3,
+    title: "Contract",
+    value: "contract",
+  },
+];

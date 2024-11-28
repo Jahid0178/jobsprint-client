6be69c39df089cc361bade4bc9fcfa79bf238049
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-28 06:05:02 +0600

    feat: Add dashboard layout and routing for job-related functionalities
    - Introduced `DashboardLayout` to structure dashboard-related pages consistently.
    - Added route `/dashboard` with redirection to `/dashboard/job-listings` for user convenience.
    - Configured `/dashboard/job-listings` to display job listings managed by `JobListingsPage`.
    - Added `/dashboard/job-listings/add` to allow job creation via `AddJobPage`.
    - Introduced `/dashboard/applied-jobs` for managing applied jobs with `AppliedJobsPage`.
    - Improved routing architecture by separating client and dashboard layouts.

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,55 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router";
 import "./index.css";
 import App from "./App.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
 import RegisterPage from "./pages/RegisterPage.tsx";
-import Header from "./components/Header/Header.tsx";
-import Footer from "./components/Footer/Footer.tsx";
+import JobListingsPage from "./pages/JobListingsPage.tsx";
+import AddJobPage from "./pages/AddJobPage.tsx";
+import DashboardLayout from "./layouts/DashboardLayout.tsx";
+import ClientLayout from "./layouts/ClientLayout.tsx";
+import AppliedJobsPage from "./pages/AppliedJobsPage.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route
-          index
-          element={<App />}
-        />
-        <Route path="auth">
+        <Route element={<ClientLayout />}>
           <Route
-            path="login"
-            element={<LoginPage />}
+            path="/"
+            element={<App />}
+          />
+          <Route path="auth">
+            <Route
+              path="/auth/login"
+              element={<LoginPage />}
+            />
+            <Route
+              path="/auth/register"
+              element={<RegisterPage />}
+            />
+          </Route>
+        </Route>
+        <Route
+          path="dashboard"
+          element={<Navigate to="/dashboard/job-listings" />}
+        />
+        <Route element={<DashboardLayout />}>
+          <Route
+            path="/dashboard/job-listings"
+            element={<JobListingsPage />}
           />
           <Route
-            path="register"
-            element={<RegisterPage />}
+            path="/dashboard/job-listings/add"
+            element={<AddJobPage />}
+          />
+          <Route
+            path="/dashboard/applied-jobs"
+            element={<AppliedJobsPage />}
           />
         </Route>
       </Routes>
-      <Footer />
     </BrowserRouter>
   </StrictMode>
 );

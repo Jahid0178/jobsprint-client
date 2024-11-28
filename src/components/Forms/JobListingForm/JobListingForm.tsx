c0a5019338ca6fd3f2726412c01facf60ab42bc0
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-28 17:27:49 +0600

    feat: add JobListingForm component

---
+++ src/components/Forms/JobListingForm/JobListingForm.tsx
@@ -0,0 +1,134 @@
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { jobLocations, jobTypes } from "@/data/data";
+import { useForm } from "react-hook-form";
+
+const JobListingForm = () => {
+  const form = useForm({
+    defaultValues: {
+      company: "",
+      position: "",
+      contract: "",
+      location: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form
+        className="space-y-4 max-w-xl mx-auto border p-4 rounded-md"
+        onSubmit={form.handleSubmit((data) => console.log(data))}
+      >
+        <FormField
+          control={form.control}
+          name="company"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Company Name</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter company name"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="position"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Position</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter position"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="location"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Location</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select location" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {jobLocations.map((location) => (
+                    <SelectItem
+                      key={location.id}
+                      value={location.value}
+                    >
+                      {location.title}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="contract"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Contract</FormLabel>
+              <Select
+                onValueChange={field.onChange}
+                defaultValue={field.value}
+              >
+                <FormControl>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Select contract type" />
+                  </SelectTrigger>
+                </FormControl>
+                <SelectContent>
+                  {jobTypes.map((jobType) => (
+                    <SelectItem
+                      key={jobType.id}
+                      value={jobType.value}
+                    >
+                      {jobType.title}
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Add Job</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default JobListingForm;

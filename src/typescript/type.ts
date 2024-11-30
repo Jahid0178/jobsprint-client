dfae1ff06088d9d5d990aeecc69255d566cd3b9a
Jahid0178 <jahidulhaque767@gmail.com>
2024-11-30 06:11:36 +0600

    feat: add types and interfaces

--- src/typescript/type.ts
+++ src/typescript/type.ts
@@ -1,5 +1,22 @@
-export type JobType = {
+export type ContractType = {
   id: number | string;
   title: string;
   value: string;
+};
+
+export type UserType = {
+  fullName: string;
+  email: string;
+  password: string;
+  role: string;
+
+  appliedJobs: JobType[];
+};
+
+export type JobType = {
+  _id?: string;
+  company: string;
+  position: string;
+  contract: string;
+  location: string;
 };
